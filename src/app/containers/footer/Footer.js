e9346b2975ed80d6818fe3c3705bb1a925746374
Nathan Tang <nathanwt@uci.edu>
2020-06-15 19:36:50 -0700

    first commit - quick trial mockup with homepage

--- src/app/containers/footer/Footer.js
+++ src/app/containers/footer/Footer.js
@@ -4,7 +4,10 @@ import "./Footer.scss";
 function Footer(props) {
   return (
     <div className="footer" style={props.style}>
-      <div>email us!!</div>
+        <div className="footer-elements container">
+            <h1>Contact Us</h1>
+            <a>HackUCI@gmail.com</a>
+        </div>
     </div>
   );
 }
