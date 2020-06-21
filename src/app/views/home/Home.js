dc5423e9b290afdd5cd069f8e39b926b87a7cad0
Nathan Tang <nathanwt@uci.edu>
2020-06-20 22:34:07 -0700

    add cards & wire assets

--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -1,43 +1,106 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./Home.scss";
+import HeaderMicroUSB from "../../../assets/micro_usb_cord_kat.png";
+import HeaderCoord from "../../../assets/cord_1_kat.png";
+import HackUCI from "../../../assets/hackuci.png";
+import ZotHacks from "../../../assets/zothacks.png";
+import HackNights from "../../../assets/hacknights.jpg";
+import Workshops from "../../../assets/workshops.jpg";
 
 function Home(props) {
   return (
     <div className="Home" style={props.style}>
-
         <div className="jumbotron jumbotron-fluid home-header">
+            <div className="home-header-usb" ><img src={HeaderMicroUSB} alt="micro_usb_coord"/></div>
+            <div className="home-header-coord"><img src={HeaderCoord} alt="usb-coord"/></div>
             <h1 className="home-header-font"><b>Hack at UCI</b></h1>
             <div className="d-flex justify-content-center">
-                <div className="involved-button"><b>Get Involved</b></div>
+                <div className="involved-button"><b><a href="#home-newsletter">Get Involved</a></b></div>
             </div>
             <div className="d-flex justify-content-center">
                 <div className="home-arrow-down"> </div>
             </div>
         </div>
 
-        <div className="container text-left home-about home-contanier">
-            <h1><b>About Us</b></h1>
-            <p>Hack at UCI is an organization that promotes developer culture by encouraging people to think creatively,
-                develop technology boldy, and pursue the leading edge of technical innovation.</p>
-            <p><a><b>Read More About Us ></b></a></p>
-        </div>
-        <div className="container text-left home-contanier">
-            <h1><b>Our Events</b></h1>
-            <p>We aim to celebrate UC Irvine's spirit of innovation by organizing ZotHacks, a beginner friendly
-                hackathon, and HackUCI, Orange County's largest hackathon. Furthermore, our organization regularly
-                host technical and professional development workshops that teach students industry-relevant skills.</p>
-            <p><a><b>View Upcoming Events ></b></a></p>
-        </div>
-        <div className="container text-left home-contanier">
-            <Carousel> </Carousel>
-        </div>
-        <div className="container text-left home-sponsors home-contanier">
-            <h1><b>Sponsors</b></h1>
-            <p>Gummi bears gingerbread danish donut danish jelly jelly beans liquorice chocolate chips. Topping poweder
-                desert oat cake. Chocolate jujubles pudding toffee candy canes. Chocolate bar candy cake icing.</p>
-            <p><a><b>View Sponsorship Information ></b></a></p>
+        <div className="home-content">
+            <div className="container text-left home-about home-container">
+                <h1><b>About Us</b></h1>
+                <p>Hack at UCI is an organization that promotes developer culture by encouraging people to think creatively,
+                    develop technology boldy, and pursue the leading edge of technical innovation.</p>
+                <p><b><Link to="/about">Read More About Us ></Link></b></p>
+            </div>
+            <div className="container text-left home-container">
+                <h1><b>Our Events</b></h1>
+                <p>We aim to celebrate UC Irvine's spirit of innovation by organizing ZotHacks, a beginner friendly
+                    hackathon, and HackUCI, Orange County's largest hackathon. Furthermore, our organization regularly
+                    host technical and professional development workshops that teach students industry-relevant skills.</p>
+                <p><b><Link to="/event">View Upcoming Events ></Link></b></p>
+            </div>
+
+            <div className="container card-container">
+                <div className="card-deck">
+                    <div className="card">
+                        <div className="card-body text-left">
+                            <img src={HackUCI} alt="HackUCI" className="card-img-top"/>
+                            <p className="card-title"><b>HackUCI</b></p>
+                            <p className="card-text">This is a short description of the event. There will be a character limit
+                            to this description so that all event cards have a consistent size. This card can fit a few sentences.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body text-left">
+                            <img src={ZotHacks} alt="ZotHacks" className="card-img-top"/>
+                            <p className="card-title"><b>ZotHacks</b></p>
+                            <p className="card-text">This is a short description of the event. There will be a character limit
+                                to this description so that all event cards have a consistent size. This card can fit a few sentences.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body text-left">
+                            <img src={HackNights} alt="HackNights" className="card-img-top"/>
+                            <p className="card-title"><b>HackNights</b></p>
+                            <p className="card-text">This is a short description of the event. There will be a character limit
+                                to this description so that all event cards have a consistent size. This card can fit a few sentences.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body text-left">
+                            <img src={Workshops} alt="Workshops" className="card-img-top"/>
+                            <p className="card-title"><b>Workshops</b></p>
+                            <p className="card-text">This is a short description of the event. There will be a character limit
+                                to this description so that all event cards have a consistent size. This card can fit a few sentences.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container text-left home-sponsors home-container">
+                <h1><b>Sponsors</b></h1>
+                <p>Gummi bears gingerbread danish donut danish jelly jelly beans liquorice chocolate chips. Topping poweder
+                    desert oat cake. Chocolate jujubles pudding toffee candy canes. Chocolate bar candy cake icing.</p>
+                <p><b><Link to="/sponsor">View Sponsorship Information ></Link></b></p>
+            </div>
+
+            <div id="home-newsletter" className="jumbotron jumbotron-fluid home-newsletter">
+                <div className="container text-left home-newsletter-container">
+                    <h1><b>Subscribe to our newsletter!</b></h1>
+                    <p> </p>
+                    <p>Get Involved! By filling your information and clicking "Subscribe", Hack at UCI will send you
+                    emails about the club's updates and events. </p>
+                    <form>
+                        <label>Email Address</label>
+                        <input type="text"/>
+                        <label>First Name</label>
+                        <input type="text"/>
+                        <label>First Name</label>
+                        <input type="text"/>
+                        <label>Graduating Year</label>
+                        <input type="text"/>
+                    </form>
+                    <p>Some text that we have to put here about Mailchimp's terms of service or something like that</p>
+                </div>
+            </div>
         </div>
     </div>
   );
