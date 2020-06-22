1f2974c4c71ba46b5f333c3e6656c1041824f231
Nathan Tang <nathanwt@uci.edu>
2020-06-21 23:28:10 -0700

    small fixes

--- src/app/views/sponsorship/Sponsorship.js
+++ src/app/views/sponsorship/Sponsorship.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 import "./Sponsorship.scss";
-import HeaderCoord from "../../../assets/cord_1_kat.png";
+import HeaderCord from "../../../assets/cord_1_kat.png";
 
 function Sponsorship(props) {
   return (
     <div className="Sponsorship" style={props.style}>
         <div className="jumbotron jumbotron-fluid sponsor-header">
-            <div className="home-header-coord"><img src={HeaderCoord} alt="usb-coord"/></div>
+            <div className="home-header-cord"><img src={HeaderCord} alt="usb-cord"/></div>
             <div className="container sponsor-header-container text-left">
                 <p className="sponsor-header-font"><b>Gummi bears gingerbread danish donut danish jelly jelly beans liquorice chocolate chips. Topping poweder
                     desert oat cake. Chocolate jujubles pudding toffee candy canes. Chocolate bar candy cake icing.</b></p>
