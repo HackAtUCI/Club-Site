a29d492299e8b9ca697a6b0e2d62dba5cd5d199e
Zachary Pinto <zpinto@uci.edu>
2020-07-08 11:51:15 -0700

    fix routing

--- server.js
+++ server.js
@@ -7,26 +7,24 @@ require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/api/fbgraph", function (req, res) {
+  axios
+    .get("https://graph.facebook.com/v7.0/me", {
+      params: {
+        fields: "events{description,end_time,name,start_time,cover}",
+        access_token: process.env.FB_PAGE_TOKEN,
+      },
+    })
+    .then((response) => {
+      res.json(response.data);
+    }).catch(function (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    });
 });
 
-app.get("/api/fbgraph", function (req, res) {
-  try {
-    axios
-      .get("https://graph.facebook.com/v7.0/me", {
-        params: {
-          fields: "events{description,end_time,name,start_time,cover}",
-          access_token: process.env.FB_PAGE_TOKEN,
-        },
-      })
-      .then((response) => {
-        res.json(response.data);
-      });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
