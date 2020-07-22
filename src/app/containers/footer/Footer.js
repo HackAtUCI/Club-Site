154b10f85d6c4821a9dcf0212eed927883fb4b01
Nathan Tang <nathanwt@uci.edu>
2020-07-22 16:39:33 -0700

    hotfixes (home): Fix margins for mailchimp section Fix mailchimp section on mobile began fix for (sponsor) began footer

--- src/app/containers/footer/Footer.js
+++ src/app/containers/footer/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Footer.scss";
 
 function Footer(props) {
@@ -8,6 +10,12 @@ function Footer(props) {
             <h1>Contact Us</h1>
             <a href="mailto:hackuci@gmail.com">HackUCI@gmail.com</a>
         </div>
+        <ul>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/event">Events</Link></li>
+            <li><Link to="/sponsor">Sponsor</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </ul>
     </div>
   );
 }
