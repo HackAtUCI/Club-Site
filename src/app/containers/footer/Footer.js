4d7fc840023204c712f62da24d927547114fd441
Nathan Tang <nathanwt@uci.edu>
2020-08-06 21:46:36 -0700

    contact label in middle

--- src/app/containers/footer/Footer.js
+++ src/app/containers/footer/Footer.js
@@ -9,7 +9,7 @@ import "./Footer.scss";
 function Footer(props) {
   return (
     <div className="footer" style={props.style}>
-        <div className="footer-elements container">
+        <div className="footer-elements">
             <p>Contact Us</p>
             <a id="HackUCI-Email" href="mailto:hackuci@gmail.com">HackUCI@gmail.com</a>
             <div>
