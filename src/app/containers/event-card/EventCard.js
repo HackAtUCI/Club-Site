ccac2f5f46666cd06cf5c1943974b9d4999c3f22
Abhinav Tumu <abhinav.tumu1@gmail.com>
2020-09-14 21:07:05 -0700

    final fixes: responsive scrolling text on modal, removed shadow padding for home cards caused by card-deck flexbox, and modal doesn't get overlayed by jumbotron bar

--- src/app/containers/event-card/EventCard.js
+++ src/app/containers/event-card/EventCard.js
@@ -39,17 +39,17 @@ function EventCard(props) {
     if (timeInterval > 0) {
       return (
         <div>
-          <Moment format="MMM D, YYYY">{props.date}</Moment>
+          <Moment format='MMM D, YYYY'>{props.date}</Moment>
         </div>
       );
     } else {
       return (
         <div>
-          <Moment format="MMM D, YYYY">{props.date}</Moment>
+          <Moment format='MMM D, YYYY'>{props.date}</Moment>
           <span> · </span>
-          <Moment format="h:mma">{props.date}</Moment>
+          <Moment format='h:mma'>{props.date}</Moment>
           <span> - </span>
-          <Moment format="h:mma">{props.end_date}</Moment>
+          <Moment format='h:mma'>{props.end_date}</Moment>
         </div>
       );
     }
@@ -57,57 +57,57 @@ function EventCard(props) {
 
   return (
     <div className={getCardType()}>
-      <div className="parent-wrapper-event-card" onClick={openModal}>
-        <div className="top-part-event-card">
+      <div className='parent-wrapper-event-card' onClick={openModal}>
+        <div className='top-part-event-card'>
           <img
-            className="top-part-event-card-img"
+            className='top-part-event-card-img'
             src={props.image}
-            alt="event"
+            alt='event'
           />
         </div>
-        <div className="bottom-part-event-card">
-          <h2 className="top-text-event-card">
+        <div className='bottom-part-event-card'>
+          <h2 className='top-text-event-card'>
             <b>{props.title}</b>
           </h2>
-          <h4 className="bottom-text-event-card">{renderBotText()}</h4>
+          <h4 className='bottom-text-event-card'>{renderBotText()}</h4>
         </div>
       </div>
       <Modal
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
-        className="modal-styling-event-card"
+        className='modal-styling-event-card'
       >
-        <div className="modal-top-part">
-          <button onClick={closeModal} className="close-button-event-card">
+        <div className='modal-top-part'>
+          <button onClick={closeModal} className='close-button-event-card'>
             X
           </button>
-          <div className="header-line-event-card">
-            <span className="popup-title-event-card">{props.title}</span>
+          <div className='header-line-event-card'>
+            <span className='popup-title-event-card'>{props.title}</span>
           </div>
-          <p className="modal-subtext-date">
+          <p className='modal-subtext-date'>
             <div>
-              <Moment format="MMM D, YYYY">{props.date}</Moment>
+              <Moment format='MMM D, YYYY'>{props.date}</Moment>
               <span> · </span>
-              <Moment format="h:mma">{props.date}</Moment>
+              <Moment format='h:mma'>{props.date}</Moment>
               <span> - </span>
-              <Moment format="h:mma">{props.end_date}</Moment>
+              <Moment format='h:mma'>{props.end_date}</Moment>
             </div>
           </p>
-          <button className="popup-button-event-card">
+          <button className='popup-button-event-card'>
             <a
               href={props.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="popup-button-event-card-link"
+              target='_blank'
+              rel='noopener noreferrer'
+              className='popup-button-event-card-link'
             >
               <h4>View on Facebook</h4>
             </a>
           </button>
         </div>
-        <div className="desc-text-wrapper-event-card">
-          <p className="desc-text-event-card">
-            <Linkify>{props.description}</Linkify>
+        <div className='desc-text-wrapper-event-card'>
+          <p className='desc-text-event-card'>
+            <Linkify> {props.description}</Linkify>
           </p>
         </div>
       </Modal>
