569553d96109d242f2021cac994247e4184ddbe6
Zachary Pinto <zpinto@uci.edu>
2020-09-17 10:24:40 -0700

    finish contact page (#15)

--- src/app/views/contact/Contact.js
+++ src/app/views/contact/Contact.js
@@ -7,55 +7,55 @@ import FacebookIcon from "../../../assets/FacebookIconPink.png";
 import InstagramIcon from "../../../assets/InstagramIconPink.png";
 import LinkedInIcon from "../../../assets/LinkedInLogoPink.png";
 
-
 function Contact(props) {
-    return (
-        <div className="Contact" style={props.style}>
-            <div className="jumbotron jumbotron-fluid global-header">
-                <div className="home-header-cord">
-                    <img src={HeaderCord} alt="usb-cord" />
-                </div>
-                <div className="global-description-container container">
-                    <p className="global-description-font">
-                        Gummi bears gingerbread danish donut danish jelly jelly beans liquorice chocolate cake.
-                        Topping powder dessert oat cake (btw, this looks scarily similar to design@uci's contact page lol):
-                        <br/>
-                        <a href="mailto:hackuci@gmail.com">HackUCI@gmail.com</a>
-                    </p>
-                </div>
-            </div>
-            <div className="contact-body align-content-center">
-                <label id="sm-title">Social Media</label>
-                <div>
-                    <div className="contact-icon d-inline-block">
-                        <a href="https://www.facebook.com/UCI.Hack/">Facebook</a>
-                        <div className="contact-icon-image">
-                            <a href="https://www.facebook.com/UCI.Hack/">
-                                <img src={FacebookIcon}/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="contact-icon d-inline-block">
-                        <a href="https://www.instagram.com/hackatuci/">Instagram</a>
-                        <div className="contact-icon-image">
-                            <a href="https://www.instagram.com/hackatuci/">
-                                <img src={InstagramIcon}/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="contact-icon d-inline-block">
-                        <a href="https://www.linkedin.com/company/hackuci">LinkedIn</a>
-                        <div className="contact-icon-image">
-                            <a href="https://www.linkedin.com/company/hackuci">
-                                <img src={LinkedInIcon}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Newsletter/>
+  return (
+    <div className="Contact" style={props.style}>
+      <div className="jumbotron jumbotron-fluid global-header">
+        <div className="home-header-cord">
+          <img src={HeaderCord} alt="usb-cord" />
         </div>
-    )
+        <div className="global-description-container container">
+          <p className="global-description-font">
+            Wondering where to get more information about Hack at UCI? Follow us
+            on Facebook, Instagram and LinkedIn to be up to date on all events,
+            workshops and hackathons. You can also reach out to us over email at{" "}
+            <a href="mailto:hackuci@gmail.com">hack@uci.edu</a>
+            <br />
+          </p>
+        </div>
+      </div>
+      <div className="contact-body align-content-center">
+        <label id="sm-title">Social Media</label>
+        <div>
+          <div className="contact-icon d-inline-block">
+            <a href="https://www.facebook.com/UCI.Hack/">Facebook</a>
+            <div className="contact-icon-image">
+              <a href="https://www.facebook.com/UCI.Hack/">
+                <img src={FacebookIcon} />
+              </a>
+            </div>
+          </div>
+          <div className="contact-icon d-inline-block">
+            <a href="https://www.instagram.com/hackatuci/">Instagram</a>
+            <div className="contact-icon-image">
+              <a href="https://www.instagram.com/hackatuci/">
+                <img src={InstagramIcon} />
+              </a>
+            </div>
+          </div>
+          <div className="contact-icon d-inline-block">
+            <a href="https://www.linkedin.com/company/hackuci">LinkedIn</a>
+            <div className="contact-icon-image">
+              <a href="https://www.linkedin.com/company/hackuci">
+                <img src={LinkedInIcon} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Newsletter />
+    </div>
+  );
 }
 
 export default Contact;
