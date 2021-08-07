45171cdc7776498211031e3ae4903937f923427d
Nathan Tang <nate.tang2@gmail.com>
2021-08-06 20:21:37 -0700

    added worker pdf

--- src/app/views/sponsorship-deck/SponsorshipDeck.js
+++ src/app/views/sponsorship-deck/SponsorshipDeck.js
@@ -4,7 +4,11 @@ import { Document, Page } from 'react-pdf';
 function SponsorshipDeck(props) {
     return (
         <div className="SponsorshipDeck" style={props.style}>
-            <Document file="Hack at UCI Sponsorship Deck 2021-2022.pdf"/>
+            <Document file="Hack at UCI Sponsorship Deck 2021-2022.pdf"
+                      options={{ workerSrc: "/pdf.worker.js" }}
+            >
+                <Page pageNumber={1} />
+            </Document>
         </div>
     );
 }
