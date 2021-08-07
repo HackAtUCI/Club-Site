f07bb4cc1e4241a5d51e37b05e2b123caade8924
Nathan Tang <nate.tang2@gmail.com>
2021-08-06 20:34:41 -0700

    enabled pdf worker

--- src/app/views/sponsorship-deck/SponsorshipDeck.js
+++ src/app/views/sponsorship-deck/SponsorshipDeck.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Document, Page } from 'react-pdf';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
 function SponsorshipDeck(props) {
     return (
         <div className="SponsorshipDeck" style={props.style}>
-            <Document file="Hack at UCI Sponsorship Deck 2021-2022.pdf"
-                      options={{ workerSrc: "/pdf.worker.js" }}
-            >
+            <Document file="Hack at UCI Sponsorship Deck 2021-2022.pdf">
                 <Page pageNumber={1} />
             </Document>
         </div>
