5977017c14567dcd4c2e1968a396675b89d37788
Taesung Hwang <taesungh@uci.edu>
2021-08-10 23:48:07 -0700

    Rebuild main navigation bar
    The navigation bar was improperly built to produce nested `<a>` tags.
    The component logic was also more complicated than necessary.
    - use proper react-bootstrap components for the navigation bar
    - replace complicated history logic with React Router's `NavLink`
    - make the navigation logo to use a NavLink instead of a regular anchor
    - specify dark variant for `Navbar` to get light collapsing icon

--- src/app/containers/app-navbar/AppNavbar.js
+++ src/app/containers/app-navbar/AppNavbar.js
@@ -1,70 +1,38 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "react-bootstrap/Navbar";
+import React from "react";
 import Nav from "react-bootstrap/Nav";
-import HackLogo from "../../../assets/hack-logo.png";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
-import { Link, useLocation } from "react-router-dom";
+import HackLogo from "assets/hack-logo.png";
 
 import "./app-navbar.scss";
 
-export default function AppNavbar() {
-  const location = useLocation();
-  const [currentPath, setcurrentPath] = useState("");
-
-  useEffect(() => {
-    setcurrentPath(location.pathname);
-  }, [location]);
-
+function AppNavbar() {
   return (
-    <Navbar className="nav-color" expand="lg">
-      <Navbar.Brand href="/">
-        <img className="nav-logo" src={HackLogo} alt="Hack at UCI" />
+    <Navbar className="navbar-color" expand="lg" variant="dark">
+      <Navbar.Brand as={NavLink} exact to="/">
+        <img className="navbar-logo" src={HackLogo} alt="Hack at UCI" />
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="iner-navbar-nav" />
+      <Navbar.Toggle aria-controls="inner-navbar-nav" />
       <Navbar.Collapse id="inner-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link>
-            <Link
-              to="/about"
-              className={currentPath.includes("about") && "selected"}
-            >
-              About
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link
-              to="/events"
-              className={currentPath.includes("events") && "selected"}
-            >
-              Events
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link
-              to="/sponsors"
-              className={currentPath.includes("sponsors") && "selected"}
-            >
-              Sponsor
-            </Link>
-          </Nav.Link>
-          {/*<Nav.Link>*/}
-          {/*  <Link*/}
-          {/*      to="/recruit"*/}
-          {/*      className={currentPath.includes("recruit") && "selected"}*/}
-          {/*  >*/}
-          {/*    Recruiting*/}
-          {/*  </Link>*/}
-          {/*</Nav.Link>*/}
-          <Nav.Link>
-            <Link
-              to="/contact"
-              className={currentPath.includes("contact") && "selected"}
-            >
-              Contact
-            </Link>
-          </Nav.Link>
+          <NavLinkItem to="/about">About</NavLinkItem>
+          <NavLinkItem to="/events">Events</NavLinkItem>
+          <NavLinkItem to="/sponsors">Sponsors</NavLinkItem>
+          {/* <NavLinkItem to="/recruit">Recruiting</NavLinkItem> */}
+          <NavLinkItem to="/contact">Contact</NavLinkItem>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 }
+
+function NavLinkItem(props) {
+  return (
+    <Nav.Item as="li">
+      <Nav.Link as={NavLink} {...props} />
+    </Nav.Item>
+  );
+}
+
+export default AppNavbar;
