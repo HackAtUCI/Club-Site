441a3670f3df45b85473dd67ef104c191baaaacf
Sam Der <sder@uci.edu>
2023-03-04 16:38:20 -0800

    Add recruitment page

--- client/src/app/containers/app-navbar/AppNavbar.js
+++ client/src/app/containers/app-navbar/AppNavbar.js
@@ -8,31 +8,31 @@ import HackLogo from "assets/hack-at-uci-logo.svg";
 import "./app-navbar.scss";
 
 function AppNavbar() {
-  return (
-    <Navbar className="navbar-color" expand="lg" variant="dark">
-      <Navbar.Brand as={NavLink} exact to="/">
-        <img className="navbar-logo" src={HackLogo} alt="Hack at UCI" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="inner-navbar-nav" />
-      <Navbar.Collapse id="inner-navbar-nav">
-        <Nav className="ml-auto">
-          <NavLinkItem to="/about">About</NavLinkItem>
-          <NavLinkItem to="/events">Events</NavLinkItem>
-          <NavLinkItem to="/sponsors">Sponsors</NavLinkItem>
-          {/* <NavLinkItem to="/recruit">Recruiting</NavLinkItem> */}
-          <NavLinkItem to="/contact">Contact</NavLinkItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+	return (
+		<Navbar className="navbar-color" expand="lg" variant="dark">
+			<Navbar.Brand as={NavLink} exact to="/">
+				<img className="navbar-logo" src={HackLogo} alt="Hack at UCI" />
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="inner-navbar-nav" />
+			<Navbar.Collapse id="inner-navbar-nav">
+				<Nav className="ml-auto">
+					<NavLinkItem to="/about">About</NavLinkItem>
+					<NavLinkItem to="/events">Events</NavLinkItem>
+					<NavLinkItem to="/sponsors">Sponsors</NavLinkItem>
+					<NavLinkItem to="/recruit">Recruitment</NavLinkItem>
+					<NavLinkItem to="/contact">Contact</NavLinkItem>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
 
 function NavLinkItem(props) {
-  return (
-    <Nav.Item as="li">
-      <Nav.Link as={NavLink} {...props} />
-    </Nav.Item>
-  );
+	return (
+		<Nav.Item as="li">
+			<Nav.Link as={NavLink} {...props} />
+		</Nav.Item>
+	);
 }
 
 export default AppNavbar;
