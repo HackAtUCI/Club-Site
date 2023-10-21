d64e42a61d4c5fef8256ecdc7417fa4a2635b109
Sam Der <sder@uci.edu>
2023-10-21 10:35:42 -0700

    update: changed "organizer" to "intern"

--- client/src/app/views/recruitment/Tech.js
+++ client/src/app/views/recruitment/Tech.js
@@ -7,12 +7,12 @@ function Tech() {
 			<h3>Technology</h3>
 			<p>
 				Every year, thousands of students across the country apply to our annual
-				HackUCI event on our website. In addition, our website also receives a
-				huge amount of traffic from various companies who visit our website to
-				obtain sponsorship information. By joining the Technology team, you not
-				only get to work on a large-scale hackathon application tracking
-				platform, but you also have the opportunity to build exciting internal
-				tools for our organizers so that they can work more efficiently.
+				hackathons on our website. In addition, our website also receives a huge
+				amount of traffic from various companies who visit our website to obtain
+				sponsorship information. By joining the Technology team, you not only
+				get to work on a large-scale hackathon application tracking platform,
+				but you also have the opportunity to build exciting internal tools for
+				our organizers so that they can work more efficiently.
 			</p>
 			<p>
 				As a general developer, your primary task is to design and develop an
