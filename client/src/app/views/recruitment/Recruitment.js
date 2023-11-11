a8819940e618e1d0f308e490a6bce3eef3944481
Sam Der <sder@uci.edu>
2023-11-11 08:49:09 -0800

    fix: remove unused import (#123)

--- client/src/app/views/recruitment/Recruitment.js
+++ client/src/app/views/recruitment/Recruitment.js
@@ -1,5 +1,4 @@
 import { Header } from "app/containers";
-import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
