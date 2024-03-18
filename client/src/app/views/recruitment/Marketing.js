17ec3156f6e621265216ea906657f92bd5116a0a
Albert Wang <alberw5@uci.edu>
2024-03-18 09:44:34 -0700

    add tab for Graphics

--- client/src/app/views/recruitment/Marketing.js
+++ client/src/app/views/recruitment/Marketing.js
@@ -5,61 +5,26 @@ function Marketing() {
 		<>
 			<img src={marketingLogo} alt="Marketing logo" />
 			<h3>Marketing</h3>
-			<div style={{ marginBottom: "3rem" }}>
-				<p>
-					<strong>General Marketing Organizer:</strong> As a marketing
-					organizer, your responsibilities vary from sending emails to making
-					posts on social media platforms in a witty and creative manner. This
-					role demands not only meeting strict deadlines but also excelling in
-					communication, as effective engagement with the public is the essence
-					of marketing.
-				</p>
-				<p>
-					<strong>Key Qualifications</strong>
-				</p>
-				<ul>
-					<li>Exceptional communication and writing skills</li>
-					<li>
-						Experience with email and social media marketing campaigns
-						(Instagram, Discord, MailChimp)
-					</li>
-					<li>Creative and critical thinking abilities</li>
-					<li>Drive to work in a fast-paced cross-functional team</li>
-					<li>Photography and videography skills preferred</li>
-				</ul>
-			</div>
-			<div style={{ marginBottom: "3rem" }}>
-				<p>
-					<strong>Graphic Designer:</strong> As a graphic designer, you will be
-					responsible for creating graphics that represent our organization and
-					events for various platforms, such as Facebook or Instagram. Designers
-					also shape the theme and aesthetic for ZotHacks and IrvineHacks. Each
-					design will follow general guidelines and have to be submitted for
-					review before given deadlines. You must be detail-oriented and have
-					proficient knowledge in visual design, color theory, and typography.
-					You will also be working closely with the web development team, so
-					experience with HTML and CSS is a plus, but not required.
-				</p>
-				<p>
-					<strong>Key Qualifications</strong>
-				</p>
-				<ul>
-					<li>Proficiency in graphic design</li>
-					<li>
-						Experience using a raster graphics editor (ex. Adobe Photoshop, Fire
-						Alpaca, etc.)
-					</li>
-					<li>Creative and critical thinking abilities</li>
-					<li>
-						Experience using a vector graphics editor (ex. Adobe Illustrator,
-						Inkscape, etc.)
-					</li>
-					<li>
-						Willing to dedicate ~4 hours per week to Hack (meeting, content
-						creation, etc.)
-					</li>
-				</ul>
-			</div>
+			<p>
+				As a marketing organizer, your responsibilities vary from sending emails
+				to making posts on social media platforms in a witty and creative
+				manner. This role demands not only meeting strict deadlines but also
+				excelling in communication, as effective engagement with the public is
+				the essence of marketing.
+			</p>
+			<p>
+				<strong>Key Qualifications</strong>
+			</p>
+			<ul>
+				<li>Exceptional communication and writing skills</li>
+				<li>
+					Experience with email and social media marketing campaigns (Instagram,
+					Discord, MailChimp)
+				</li>
+				<li>Creative and critical thinking abilities</li>
+				<li>Drive to work in a fast-paced cross-functional team</li>
+				<li>Photography and videography skills preferred</li>
+			</ul>
 		</>
 	);
 }
