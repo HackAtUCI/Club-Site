dfb75e00cae38c0ff09052250e2ae8dfeec688d9
Noah Kim <noahmk5002@gmail.com>
2025-05-08 22:36:47 -0700

    Change background colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import {
+	Route,
+	Switch,
+	BrowserRouter as Router,
+	useLocation,
+} from "react-router-dom";
 
 import { AppNavbar, Footer } from "app/containers";
 import {
@@ -16,42 +21,58 @@ import "./App.scss";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+function AppWrapper() {
+	const location = useLocation();
+
+	console.log(location.pathname);
+
+	// Home page and about page have background gradients in the Figma file.
+	// If either of the routes are selected, then add the appropriate css class.
+	let cn = "App";
+	if (location.pathname === "/") {
+		cn += " home-background";
+	} else if (location.pathname === "/about") {
+		cn += " about-background";
+	}
+
+	return (
+		<div className={cn}>
+			<AppNavbar />
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/about" component={About} />
+				<Route exact path="/events" component={Events} />
+				<Route exact path="/sponsors" component={Sponsorship} />
+				<Route exact path="/recruit" component={Recruitment} />
+				<Route exact path="/contact" component={Contact} />
+				<Route
+					exact
+					path="/mentor"
+					component={() => {
+						window.location.href = "https://hack.ics.uci.edu/contact/#mentors";
+					}}
+				/>
+				<Route
+					exact
+					path="/sponsorship-deck"
+					component={() => {
+						window.location.href =
+							"Hack%20at%20UCI%20Sponsorship%20Deck%202021-2022.pdf";
+						return null;
+					}}
+				/>
+				<Route component={NotFound} />
+			</Switch>
+			<Footer />
+		</div>
+	);
+}
+
 function App() {
 	return (
-		<div className="App">
-			<Router>
-				<div>
-					<AppNavbar />
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/about" component={About} />
-						<Route exact path="/events" component={Events} />
-						<Route exact path="/sponsors" component={Sponsorship} />
-						<Route exact path="/recruit" component={Recruitment} />
-						<Route exact path="/contact" component={Contact} />
-						<Route
-							exact
-							path="/mentor"
-							component={() => {
-								window.location.href =
-									"https://hack.ics.uci.edu/contact/#mentors";
-							}}
-						/>
-						<Route
-							exact
-							path="/sponsorship-deck"
-							component={() => {
-								window.location.href =
-									"Hack%20at%20UCI%20Sponsorship%20Deck%202021-2022.pdf";
-								return null;
-							}}
-						/>
-						<Route component={NotFound} />
-					</Switch>
-					<Footer />
-				</div>
-			</Router>
-		</div>
+		<Router>
+			<AppWrapper />
+		</Router>
 	);
 }
 
