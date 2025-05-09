7af0d78f7f684ca6b4807fc13aa26c6bfd6cb21e
Noah Kim <noahmk5002@gmail.com>
2025-05-09 00:14:52 -0700

    Configure hero page

--- client/src/app/containers/header/Header.js
+++ client/src/app/containers/header/Header.js
@@ -1,18 +1,43 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
 
 import "./Header.scss";
+import PrimaryButton from "app/components/primary-button/PrimaryButton";
 
-function Header({ title, children }) {
-  return (
-    <header className="global-header">
-      <div className="header-image">
-        <div className="header-overlay">
-          <h1>{title}</h1>
-          {children}
-        </div>
-      </div>
-    </header>
-  );
+import LogoWhite from "assets/header-logo/logo-white.png";
+import LogoShadowBlue from "assets/header-logo/logo-shadow-blue.png";
+import LogoShadowGreen from "assets/header-logo/logo-shadow-green.png";
+
+function Header({ title }) {
+	return (
+		<header className="global-header">
+			<Logo />
+			<div className="header-items">
+				<h1>{title}</h1>
+				<PrimaryButton href="#newsletter" text="Get Involved" />
+			</div>
+		</header>
+	);
+}
+
+// Logo with blue and green shadows
+function Logo() {
+	return (
+		<div className="logo-wrapper">
+			<Image
+				src={LogoShadowGreen}
+				alt="Hack at UCI logo"
+				className="logo-shadow-green"
+			/>
+			<Image
+				src={LogoShadowBlue}
+				alt="Hack at UCI logo"
+				className="logo-shadow-blue"
+			/>
+
+			<Image src={LogoWhite} alt="Hack at UCI logo" className="logo-white" />
+		</div>
+	);
 }
 
 export default Header;
