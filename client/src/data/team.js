c80d57d9edd06ab8ed7b1c3905c45e74ea5d680b
Nathan Ong <164730140+NoNathan17@users.noreply.github.com>
2025-12-29 20:44:40 -0800

    Team Section on New Site (#181)
    * implemented team section on about
    
    * added images, linkedins, and team statement

---
+++ client/src/data/team.js
@@ -0,0 +1,253 @@
+export const teamMembers = {
+	Communications: [
+		{
+			name: "Arnav Nigam",
+			position: "Communications Director",
+			image: "/team/arnavn.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/just-arnav/",
+		},
+	],
+
+	Corporate: [
+		{
+			name: "Alex Ngo",
+			position: "Corporate Director",
+			image: "/team/alex.jpeg",
+			linkedInUrl: "https://www.linkedin.com/in/alexryanngo/",
+		},
+		{
+			name: "Paige Townsend",
+			position: "Corporate Director",
+			image: "/team/paige.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/ptownse/",
+		},
+		{
+			name: "Eleni Arapostathis",
+			position: "Corporate Organizer",
+			image: "/team/eleni.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/eleni-arapostathis-9b25a0369/",
+		},
+		{
+			name: "Rishi Potipreddi",
+			position: "Corporate Organizer",
+			image: "/team/riship.jpeg",
+			linkedInUrl: "https://www.linkedin.com/in/rishi-potipireddi/",
+		},
+		{
+			name: "Rishita Dugar",
+			position: "Corporate Organizer",
+			image: "/team/rishita.png",
+			linkedInUrl: "https://www.linkedin.com/in/rishita-dugar/",
+		},
+		{
+			name: "Shea Thomopoulos",
+			position: "Corporate Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/shea-anastasia-thomopoulos/",
+		},
+		{
+			name: "Trystan Vasquez",
+			position: "Corporate Organizer",
+			image: "/team/trystan.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/trystan-camden-vasquez/",
+		},
+	],
+
+	Logistics: [
+		{
+			name: "Angelina Wang",
+			position: "Logistics Director",
+			image: "/team/angelina.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/angelina-wangg/",
+		},
+		{
+			name: "Sharmada Honnavalli",
+			position: "Logistics Director",
+			image: "/team/sharmada.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/sharmada-honnavalli/",
+		},
+		{
+			name: "Zion Mateo",
+			position: "Logistics Director",
+			image: "/team/zion.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/zionmateo/",
+		},
+		{
+			name: "Arnav Pandey",
+			position: "Logistics Organizer",
+			image: "/team/arnavp.png",
+			linkedInUrl: "https://www.linkedin.com/in/arnav-pandey-985280227/",
+		},
+		{
+			name: "Connie Yang",
+			position: "Logistics Organizer",
+			image: "/team/connie.png",
+			linkedInUrl: "https://www.linkedin.com/in/coyang/",
+		},
+		{
+			name: "Ethan Chin",
+			position: "Logistics Organizer",
+			image: "/team/ethan.webp",
+			linkedInUrl: "https://www.linkedin.com/in/ethan-chin/",
+		},
+		{
+			name: "Grace Wang",
+			position: "Logistics Organizer",
+			image: "/team/grace.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/grace-w-39b7131b1/",
+		},
+		{
+			name: "Joshua Sullivan",
+			position: "Logistics Organizer",
+			image: "/team/josh.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/joshua-micah-sullivan/",
+		},
+		{
+			name: "Kailey Trinh",
+			position: "Logistics Organizer",
+			image: "/team/kailey.png",
+			linkedInUrl: "https://www.linkedin.com/in/kailey-trinh-5182a8244/",
+		},
+		{
+			name: "Yousef Khan",
+			position: "Logistics Organizer",
+			image: "/team/yousef.jpeg",
+			linkedInUrl: "https://www.linkedin.com/in/theyousefkhan/",
+		},
+	],
+
+	Marketing: [
+		{
+			name: "Pavana Upadhyaya",
+			position: "Marketing Director",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/pavana-upadhyaya/",
+		},
+		{
+			name: "Jazib Rahim",
+			position: "Marketing Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "",
+		},
+		{
+			name: "Lavanya Agarwal",
+			position: "Marketing Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/lavanya-agarwal-25a56933b/",
+		},
+		{
+			name: "Pragya Jhunjhunwala",
+			position: "Marketing Organizer",
+			image: "/team/pragya.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/pragya-jhunjhunwala/",
+		},
+		{
+			name: "Rithika Yalla",
+			position: "Marketing Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/rithika-yalla-96a490328/",
+		},
+		{
+			name: "Sejal Pawar",
+			position: "Marketing Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/sejal-pawar-3abb7a280/",
+		},
+		{
+			name: "Tiffany Cheung",
+			position: "Marketing Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/tifffanycheung/",
+		},
+		{
+			name: "Tracy Newberry",
+			position: "Marketing Organizer",
+			image: "/team/tracy.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/tracy-ann-newberry-5a348632a/",
+		},
+	],
+
+	Graphics: [
+		{
+			name: "Camille Oh",
+			position: "Graphics Director",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/camilleoh/",
+		},
+		{
+			name: "Annabel Wang",
+			position: "Graphics Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/annabel-wang-794911298/",
+		},
+		{
+			name: "Audrey Chen",
+			position: "Graphics Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/audrey-loyola-chen-6233b6296/",
+		},
+		{
+			name: "Elijah Smith",
+			position: "Graphics Organizer",
+			image: "/team/elijah.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/elijah-smith-71319429a/",
+		},
+		{
+			name: "Julia Modina",
+			position: "Graphics Organizer",
+			image: "/team/julia.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/julia-modina/",
+		},
+	],
+
+	Tech: [
+		{
+			name: "Ian Dai",
+			position: "Tech Director",
+			image: "/team/ian.jpg",
+			linkedInUrl: "https://www.linkedin.com/in/ian-dai/",
+		},
+		{
+			name: "Andrew Hwang",
+			position: "Tech Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/andrew-hwang-78b375258/",
+		},
+		{
+			name: "Cyril Joby",
+			position: "Tech Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/cyril-joby/",
+		},
+		{
+			name: "Jay Wu",
+			position: "Tech Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/jayqw/",
+		},
+		{
+			name: "Nathan Ong",
+			position: "Tech Organizer",
+			image: "/team/nathan.png",
+			linkedInUrl: "https://www.linkedin.com/in/nathan-ong17/",
+		},
+		{
+			name: "Noah Kim",
+			position: "Tech Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/noahkim004/",
+		},
+		{
+			name: "Rishi Murumkar",
+			position: "Tech Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/rishi-murumkar/",
+		},
+		{
+			name: "Vishok Lakshmankumar",
+			position: "Tech Organizer",
+			image: "/team/blank.png",
+			linkedInUrl: "https://www.linkedin.com/in/vishokl/",
+		},
+	],
+};
