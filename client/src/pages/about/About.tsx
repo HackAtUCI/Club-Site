591fea0ee5e8b9334df5a98dae32ccd43a44c7b2
ianbb <ianbbdai@gmail.com>
2025-12-31 15:32:40 -0800

    Added bare hero section

--- client/src/pages/about/About.tsx
+++ client/src/pages/about/About.tsx
@@ -1,9 +1,11 @@
-import TeamSection from './components/TeamSection';
+import Hero from "./components/Hero";
+import TeamSection from "./components/TeamSection";
 
 export default function About() {
 	return (
-	<>
-		<TeamSection />
-	</>
+		<>
+			<Hero />
+			<TeamSection />
+		</>
 	);
 }
---
+++ client/src/pages/about/components/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+	return (
+		<section className="h-screen flex justify-around items-center">
+			<div className="max-w-4/11">
+				<h1>about us</h1>
+			</div>
+
+			<div className="flex flex-col gap-3 max-w-4/11">
+				<p>Hack at UCI is Irvine’s premiere collegiate hackathon committee.</p>
+				<p>
+					We strive to unite students passionate about hacking and retaining the
+					spirit of community.
+				</p>
+				<p>Let’s take the next step</p>
+			</div>
+		</section>
+	);
+}
