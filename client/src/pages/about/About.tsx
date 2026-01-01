951ccd6b2a43da18b5f48aea4b980b0fb47c00f8
ianbb <ianbbdai@gmail.com>
2025-12-31 16:12:22 -0800

    Created OurMission section

--- client/src/pages/about/About.tsx
+++ client/src/pages/about/About.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
+import OurMission from "./components/OurMission";
 import TeamSection from "./components/TeamSection";
 
 export default function About() {
 	return (
 		<>
 			<Hero />
+			<OurMission />
 			<TeamSection />
 		</>
 	);
