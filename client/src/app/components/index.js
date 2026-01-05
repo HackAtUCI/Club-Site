ecad2e35ec5acc46dfe3589ea343ead3015c73dc
ianbb <ianbbdai@gmail.com>
2026-01-05 01:05:27 -0800

    Reapply "Overhaul of site: Vite, Tailwind, Typescript, and New Design (#163)"
    This reverts commit 993f8b90df67d1c185239bdeca6383589e128c64.

--- client/src/app/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Newsletter } from "./newsletter/Newsletter";
-export { default as CurrentSponsors } from "./sponsors/CurrentSponsors";
---
+++ client/src/pages/events/Events.tsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+import Hackathons from "./components/Hackathons";
+
+export default function Events() {
+	return (
+		<>
+			<Hero />
+			<Hackathons />
+		</>
+	);
+}
