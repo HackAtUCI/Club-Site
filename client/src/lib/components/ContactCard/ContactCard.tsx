4ffcc5459dcd6db23175e37605d6fbef369c5de7
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2026-01-05 01:07:14 -0800

    New Site Release (#195)
    * Added 404 from Next repo (#172)
    
    * Updated README files
    
    * Added recruitment button and controls (#176)
    
    * Added boolean to control apply buttons for recruitment
    
    * Removed comments
    
    * Added id for buttons
    
    * Closed recruitment
    
    * Implemented Contact Us page (#177)
    
    * Implemented Contact Us page
    
    * Cleaned up clearing form and fixed submission
    
    * Removed alert, confirmation comes from mailchimp itself
    
    * Spaced inputs
    
    * Adjusted contact card text centering
    
    ---------
    
    Co-authored-by: ianbb <ianbbdai@gmail.com>
    
    * Add back to top button (#179)
    
    * Team Section on New Site (#181)
    
    * implemented team section on about
    
    * added images, linkedins, and team statement
    
    * Added contact to home page (#182)
    
    * Moved ContactCard to own component
    
    * Added force mobile view
    
    * Moved ContactCard to shared components
    
    * Created contact section for home
    
    * Refactored scroll
    
    * Reordered imports
    
    * Chore: removed extra line
    
    * Fixed primary button size to shrink in mobile
    
    * Added AboutUs Hero and Mission (#185)
    
    * Added bare hero section
    
    * Added images
    
    * Created OurMission section
    
    * Shrunk OurMission padding
    
    * Moved hero components
    
    * Added back white linkedin to contact page
    
    * Shrunk button padding for hero on home page on mobile (#186)
    
    * Created ScrollToTop component for whole app
    
    * Revert "Created ScrollToTop component for whole app"
    
    This reverts commit 9d7521ed5410fc508fd767dc36673f9ac42ba9fb.
    
    * Final bug fixes (#191)
    
    * Reapply "Created ScrollToTop component for whole app"
    
    This reverts commit 2be05bacb4cc1ae89c88e5f7fabbbbb28890812c.
    
    * Refactored PrimaryAnchor to use anchor when in external site
    
    * Refactored ContactIcon into own component
    
    * chore: fixed imports and removed unused style
    
    * Added className prop to ContactIcon
    
    * Feat/events final touches (Without photos or numbers) (#180)
    
    * Events tab finishes
    
    * Refactored events tabs style into tabs
    
    * Updated stats
    
    * Fixed text
    
    * Added images to events
    
    * Moved dot class
    
    ---------
    
    Co-authored-by: ianbb <ianbbdai@gmail.com>
    
    * Fixed recruitment tabs (#192)
    
    * Fix stretching on large screens (#194)
    
    * Added container class to home page
    
    * Added container class to about
    
    * Fixed margin for recruitment
    
    * Added and resized images to home page
    
    * Used normal buttons for events
    
    ---------
    
    Co-authored-by: VishokL <73354883+VishokL@users.noreply.github.com>
    Co-authored-by: Noah Kim <101167833+noahk004@users.noreply.github.com>
    Co-authored-by: Nathan Ong <164730140+NoNathan17@users.noreply.github.com>
    Co-authored-by: Andrew Hwang <148009680+pandyrew@users.noreply.github.com>

---
+++ client/src/lib/components/ContactCard/ContactCard.tsx
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+
+import { UnspacedBox } from "@/lib/components/Box/Box";
+import PrimaryButton from "@/lib/components/PrimaryButton/PrimaryButton";
+
+import "./ContactCard.css";
+
+interface ContactCardProps {
+	title: string;
+	buttonText: string;
+	href?: string;
+	onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+	icon: string;
+	forceMobile?: boolean;
+}
+
+export default function ContactCard({
+	title,
+	buttonText,
+	href,
+	onClick,
+	icon,
+	forceMobile = false,
+}: ContactCardProps) {
+	return (
+		<UnspacedBox
+			className={clsx("contact-card", forceMobile && "force-mobile")}
+		>
+			<div className="contact-card-icon">
+				<img src={icon} alt={title} />
+			</div>
+
+			<div className="contact-card-content">
+				<h2>{title}</h2>
+				<PrimaryButton href={href} onClick={onClick}>
+					{buttonText}
+				</PrimaryButton>
+			</div>
+		</UnspacedBox>
+	);
+}
