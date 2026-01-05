0f4e67bfc16436e53f2415434269a8d4464dfd12
ianbb <ianbbdai@gmail.com>
2026-01-04 23:14:31 -0800

    Refactored events tabs style into tabs

--- client/src/lib/components/Tabs/Tabs.tsx
+++ client/src/lib/components/Tabs/Tabs.tsx
@@ -8,41 +8,51 @@ export interface TabInterface {
 }
 
 interface TabsInterface {
+	title?: string;
 	tabs: TabInterface[];
 	useAltButton?: boolean;
 }
 
-export default function Tabs({ tabs, useAltButton = false }: TabsInterface) {
+export default function Tabs({
+	title,
+	tabs,
+	useAltButton = false,
+}: TabsInterface) {
 	const [activeTab, setActiveTab] = useState<TabInterface | undefined>(tabs[0]);
 
 	return (
-		<div>
-			<div className="my-12 flex items-center justify-center">
-				{tabs.map((tab: TabInterface) =>
-					useAltButton ? (
-						<a
-							key={tab.buttonText}
-							onClick={() => setActiveTab(tab)}
-							className={`anchor-color text-4xl ${
-								activeTab?.buttonText === tab.buttonText ? "active" : ""
-							} `}
-						>
-							{tab.buttonText}
-						</a>
-					) : (
-						<button
-							key={tab.buttonText}
-							onClick={() => setActiveTab(tab)}
-							className={`tab-button ${
-								activeTab?.buttonText === tab.buttonText ? "active" : ""
-							} `}
-						>
-							{tab.buttonText}
-						</button>
-					)
-				)}
+		<>
+			<div className="bg-[#2D2D3E] p-6 pb-12 sm:p-12 sm:pb-16 md:p-16 md:pb-20 lg:p-20 lg:pb-24">
+				{title && <h2 className="text-subtitle text-center">{title}</h2>}
+				<div className="mt-6 sm:mt-8 md:mt-10 lg:mt-12 flex items-center justify-center flex-wrap gap-2 sm:gap-4">
+					{tabs.map((tab: TabInterface) =>
+						useAltButton ? (
+							<a
+								key={tab.buttonText}
+								onClick={() => setActiveTab(tab)}
+								className={`anchor-color text-4xl ${
+									activeTab?.buttonText === tab.buttonText ? "active" : ""
+								} `}
+							>
+								{tab.buttonText}
+							</a>
+						) : (
+							<button
+								key={tab.buttonText}
+								onClick={() => setActiveTab(tab)}
+								className={`tab-button ${
+									activeTab?.buttonText === tab.buttonText ? "active" : ""
+								} `}
+							>
+								{tab.buttonText}
+							</button>
+						)
+					)}
+				</div>
 			</div>
-			<div className="relative">{activeTab?.tabComponent}</div>
-		</div>
+			<div className="relative -mt-10 sm:-mt-12 md:-mt-16 lg:-mt-20">
+				{activeTab?.tabComponent}
+			</div>
+		</>
 	);
 }
--- client/src/pages/events/components/Hackathons.tsx
+++ client/src/pages/events/components/Hackathons.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
 import IrvineHacksTab from "./IrvineHacksTab";
 import ZotHacksTab from "./ZotHacksTab";
-import "@/lib/components/Tabs/Tabs.css";
+import Tabs from "@/lib/components/Tabs/Tabs";
 
 type HackathonTab = "IrvineHacks" | "ZotHacks";
 
@@ -20,33 +19,5 @@ const hackathonTabs: {
 ];
 
 export default function Hackathons() {
-	const [activeTab, setActiveTab] = useState<HackathonTab>("IrvineHacks");
-
-	const activeTabComponent = hackathonTabs.find(
-		(tab) => tab.buttonText === activeTab
-	)?.tabComponent;
-
-	return (
-		<>
-			<div className="bg-[#2D2D3E] p-6 pb-12 sm:p-12 sm:pb-16 md:p-16 md:pb-20 lg:p-20 lg:pb-24">
-				<h2 className="text-subtitle text-center">Our Hackathons</h2>
-				<div className="mt-6 sm:mt-8 md:mt-10 lg:mt-12 flex items-center justify-center flex-wrap gap-2 sm:gap-4">
-					{hackathonTabs.map((tab) => (
-						<button
-							key={tab.buttonText}
-							onClick={() => setActiveTab(tab.buttonText)}
-							className={`tab-button ${
-								activeTab === tab.buttonText ? "active" : ""
-							} `}
-						>
-							{tab.buttonText}
-						</button>
-					))}
-				</div>
-			</div>
-			<div className="relative -mt-10 sm:-mt-12 md:-mt-16 lg:-mt-20">
-				{activeTabComponent}
-			</div>
-		</>
-	);
+	return <Tabs tabs={hackathonTabs} title="Our Hackathons" useAltButton />;
 }
