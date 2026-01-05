c5a3737432081fff5ea64365e9737c7780c80c88
Andrew Hwang <148009680+pandyrew@users.noreply.github.com>
2026-01-05 00:21:01 -0800

    Feat/events final touches (Without photos or numbers) (#180)
    * Events tab finishes
    
    * Refactored events tabs style into tabs
    
    * Updated stats
    
    * Fixed text
    
    * Added images to events
    
    * Moved dot class
    
    ---------
    
    Co-authored-by: ianbb <ianbbdai@gmail.com>

--- client/src/pages/events/components/ZotHacksTab.tsx
+++ client/src/pages/events/components/ZotHacksTab.tsx
@@ -1,38 +1,50 @@
-import Box from "@/lib/components/Box/Box";
 import StatBox from "./StatBox";
 import Carousel from "@/lib/components/Carousel/Carousel";
-import IrvineHacksImg from "@/assets/images/IrvineHacks_img.png";
-import ZotHacksImg from "@/assets/images/ZotHacks_img.png";
+import ZH1 from "@/assets/images/ZH1-resized.jpg";
+import ZH2 from "@/assets/images/ZH2-resized.jpg";
+import ZH3 from "@/assets/images/ZH3-resized.jpg";
 
 export default function ZotHacksTab() {
 	return (
-		<Box alt>
-			<div className="flex w-full">
-				<div className="flex w-1/2 flex-col pr-8">
-					<h2 className="text-subtitle mb-8">ZotHacks</h2>
-					<p>
-						ZotHacks is a beginner-friendly hackathon where students with
-						minimal computer science experience will learn new skills, take part
-						in a community of creative people, and build projects!
-					</p>
-				</div>
-				<div className="w-1/2">
-					<Carousel
-						options={{ loop: true }}
-						dotsClassName="mt-8 flex justify-center"
-					>
-						<img src={IrvineHacksImg} alt="IrvineHacks" className="w-full" />
-						<img src={ZotHacksImg} alt="ZotHacks" className="w-full" />
-						<img src={ZotHacksImg} alt="ZotHacks" className="w-full" />
-					</Carousel>
+		<div className="w-full p-4 sm:p-6 md:p-8 lg:p-12">
+			<div className="bg-linear-to-b from-green-300 to-blue-400 p-2 rounded-4xl	">
+				<div className="bg-dark-blue rounded-3xl p-4 sm:p-6 md:p-8 lg:p-10">
+					<div className="flex flex-col lg:flex-row w-full gap-6 lg:gap-0">
+						<div className="flex w-full lg:w-1/2 flex-col lg:pr-8">
+							<h2 className="text-subtitle mb-4 sm:mb-6 md:mb-8">ZotHacks</h2>
+							<p>
+								ZotHacks is a beginner-friendly hackathon where students with
+								minimal computer science experience will learn new skills, take
+								part in a community of creative people, and build projects!
+							</p>
+						</div>
+						<div className="w-full lg:w-1/2">
+							<Carousel options={{ loop: true }}>
+								<img
+									src={ZH1}
+									alt="ZotHacks"
+									className="object-cover aspect-3/2"
+								/>
+								<img
+									src={ZH2}
+									alt="ZotHacks"
+									className="object-cover aspect-3/2"
+								/>
+								<img
+									src={ZH3}
+									alt="ZotHacks"
+									className="object-cover aspect-3/2"
+								/>
+							</Carousel>
+						</div>
+					</div>
+					<div className="mt-6 sm:mt-8">
+						<StatBox boxText="60" text="students" />
+						<StatBox boxText="19" text="majors" />
+						<StatBox boxText="15" text="projects submitted" />
+					</div>
 				</div>
 			</div>
-			<div className="mt-8">
-				<StatBox count={10} text="attendees" />
-				<StatBox count={10} text="colleges" />
-				<StatBox count={10} text="first-timers" />
-				<StatBox count={10} text="projects submitted" />
-			</div>
-		</Box>
+		</div>
 	);
 }
