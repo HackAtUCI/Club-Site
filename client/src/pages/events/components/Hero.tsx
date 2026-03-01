0c40cf8d9d2b1754342e0aed531e26b52598f7a4
Nathan Ong <nathanong1217@gmail.com>
2026-03-01 00:28:25 -0800

    fix: mobile fixes

--- client/src/pages/events/components/Hero.tsx
+++ client/src/pages/events/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
 	return (
-		<section className="flex h-[60vh] sm:h-screen flex-col items-center justify-center gap-12">
-			<h1 className="text-heading">Events</h1>
+		<section className="flex h-screen flex-col items-center justify-center gap-12">
+			<h1 className="text-5xl font-bold md:text-heading">Events</h1>
 		</section>
 	);
 }
--- client/src/pages/recruitment/components/Hero.tsx
+++ client/src/pages/recruitment/components/Hero.tsx
@@ -7,8 +7,8 @@ export default function Hero({
 }) {
 	return (
 		<section className="flex flex-col justify-center items-center gap-6 h-screen">
-			<h1 className="text-heading">Recruitment</h1>
-			<p> Learn more about Hack and apply to be an organizer here ↓</p>
+			<h1 className="text-5xl font-bold md:text-heading">Recruitment</h1>
+			<p className="text-center"> Learn more about Hack and apply to be an organizer here ↓</p>
 			{showApplyButton ? (
 				<PrimaryButton href="https://www.notion.so/hackatuci/Hack-at-UCI-Spring-2026-Organizer-Recruitment-311393c84e5d80fabf9eea424ddee5c7?source=copy_link">
 					Click me!
