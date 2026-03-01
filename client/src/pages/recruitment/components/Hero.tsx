b7b2dfad906b9a2cff7879af5db9bc674145bacf
Nathan Ong <nathanong1217@gmail.com>
2026-03-01 00:47:58 -0800

    fix: mobile for event and recruitment header

--- client/src/pages/recruitment/components/Hero.tsx
+++ client/src/pages/recruitment/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero({
 }) {
 	return (
 		<section className="flex flex-col justify-center items-center gap-6 h-screen">
-			<h1 className="text-5xl font-bold md:text-heading">Recruitment</h1>
+			<h1 className="text-5xl font-bold md:text-8xl">Recruitment</h1>
 			<p className="text-center"> Learn more about Hack and apply to be an organizer here ↓</p>
 			{showApplyButton ? (
 				<PrimaryButton href="https://www.notion.so/hackatuci/Hack-at-UCI-Spring-2026-Organizer-Recruitment-311393c84e5d80fabf9eea424ddee5c7?source=copy_link">
