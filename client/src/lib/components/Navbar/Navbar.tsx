7ebc52f283b4c3b53bd26e18da1c83c00681e403
Julianna Alderete <jyaldere@uci.edu>
2026-04-30 12:12:15 -0700

    fix: logo, navLinks, CTA spacing

--- client/src/lib/components/Navbar/Navbar.tsx
+++ client/src/lib/components/Navbar/Navbar.tsx
@@ -29,24 +29,24 @@ export default function BaseNavbar({ children }: PropsWithChildren) {
 	return (
 		<NavMenu.Root
 			className={`${hasScrolledFromTop ? "opacity-0" : "opacity-100"
-               } hidden md:flex items-center justify-center fixed px-[50px] py-[25px] my-20 left-1/2 -translate-x-1/2 z-50 w-[700px] h-[60px] rounded-[25px] border border-[1.5px] border-white bg-black transition-opacity duration-300 ease-out`}
+               } hidden md:flex items-center justify-center fixed px-12 py-6 my-20 left-1/2 -translate-x-1/2 z-50 w-[700px] h-[60px] rounded-[25px] border border-[1.5px] border-white bg-black transition-opacity duration-300 ease-out`}
 		>
-
-			<NavMenu.List className="grid grid-cols-3 items-center w-full px-6">
-				<div className="justify-self-start">
+			<NavMenu.List className="flex justify-between items-center w-full gap-32">
+				<div className="flex-shrink-0">
 					<NavLinkItem to="/">
 						<img src={HackLogo} alt="Hack at UCI Logo" width={34} height={34} />
 					</NavLinkItem>
 				</div>
 				
-				<div className="flex justify-center items-center gap-10 pt-2">
+				<div className="flex items-center gap-10 pt-2">
 					<NavLinkItem to="/about">About</NavLinkItem>
 					<NavLinkItem to="/events">Events</NavLinkItem>
 					<NavLinkItem to="/recruitment">Recruitment</NavLinkItem>
 				</div>
-				<div className="justify-self-end ml-auto flex px-6 pt-3 pb-1 bg-[#333333] rounded-[12px]">
-					<NavLinkItem to="/contact" className="flex justify-center items-center leading-none"
-					>Join Us</NavLinkItem>
+				<div className="flex-shrink-0 flex px-6 pt-2 bg-[#333333] rounded-xl">
+					<NavLinkItem to="/contact">
+						Join Us
+					</NavLinkItem>
 				</div>
 
 				{children}
