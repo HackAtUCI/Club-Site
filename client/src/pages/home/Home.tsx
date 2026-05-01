2e448477aedad63f85e23a921f8e4091c1a69880
Joshua Micah Sullivan <joshee.sullivan@gmail.com>
2026-05-01 12:05:02 -0700

    feat(home): condensed home decor into separate component

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -7,11 +7,7 @@ import Sponsors from "@/lib/components/Home/Sponsors/Sponsors";
 import Contact from "@/lib/components/Home/Contact/Contact";
 
 import UpArrow from "@/assets/icons/up-arrow.svg";
-import bigCircleLeft from "@/assets/hero-decor/big-circle-left.svg";
-import bigCircleRight from "@/assets/hero-decor/big-circle-right.svg";
-import smallCircle from "@/assets/hero-decor/small-circle.svg";
-import heroVectorLeft from "@/assets/hero-decor/hero-vector-left.svg";
-import heroVectorRight from "@/assets/hero-decor/hero-vector-right.svg";
+import HomeDecor from "./HomeDecor";
 
 export default function Home() {
 	const heroRef = useRef<HTMLDivElement>(null);
@@ -27,57 +23,10 @@ export default function Home() {
 
 	return (
 		<div className="min-h-screen overflow-x-hidden">
-			{/* Shared decor layer for Hero + Events so elements can span sections */}
-			<div className="relative isolate overflow-visible">
-				{/* Shared light background behind Hero + Events */}
-				<div
-					className="pointer-events-none absolute inset-x-0 top-0 z-0 h-[calc(100svh+52rem)] hack-background-gradient"
-					aria-hidden
-				/>
-
-				<img
-					src={bigCircleLeft}
-					alt=""
-					aria-hidden
-					draggable={false}
-					className="pointer-events-none absolute top-35 left-30 z-10 w-[clamp(220px,24vw,520px)] -translate-x-[34%] -translate-y-[38%] select-none opacity-80"
-				/>
-				<img
-					src={smallCircle}
-					alt=""
-					aria-hidden
-					draggable={false}
-					className="pointer-events-none absolute top-92 left-82 z-10 w-[clamp(32px,7vw,120px)] -translate-x-[34%] -translate-y-[38%] select-none opacity-80"
-				/>
-				<img
-					src={heroVectorRight}
-					alt=""
-					aria-hidden
-					draggable={false}
-					className="pointer-events-none absolute top-0 right-0 z-10 w-[clamp(200px,29vw,440px)] translate-x-[30%] -translate-y-[10%] select-none"
-				/>
-
-				{/* Spillover assets: anchor near Hero bottom so they remain visible into Events */}
-				<img
-					src={heroVectorLeft}
-					alt=""
-					aria-hidden
-					draggable={false}
-					className="pointer-events-none absolute left-0 top-[calc(100svh+2rem)] z-10 w-[clamp(200px,29vw,440px)] -translate-x-[20%] select-none"
-				/>
-				<img
-					src={bigCircleRight}
-					alt=""
-					aria-hidden
-					draggable={false}
-					className="pointer-events-none absolute right-38 top-[calc(100svh+6rem)] z-10 w-[clamp(320px,30vw,760px)] translate-x-[36%] select-none opacity-70"
-				/>
-
-				<div className="relative z-20">
-					<Hero ref={heroRef} onClick={() => scrollToRef({ ref: contactRef })} />
-					<Events />
-				</div>
-			</div>
+			<HomeDecor>
+				<Hero ref={heroRef} onClick={() => scrollToRef({ ref: contactRef })} />
+				<Events />
+			</HomeDecor>
 			<div className="home-background">
 				<Contact ref={contactRef} />
 				<Sponsors />
---
+++ client/src/pages/home/HomeDecor.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import bigCircleLeft from "@/assets/hero-decor/big-circle-left.svg";
+import bigCircleRight from "@/assets/hero-decor/big-circle-right.svg";
+import smallCircle from "@/assets/hero-decor/small-circle.svg";
+import heroVectorLeft from "@/assets/hero-decor/hero-vector-left.svg";
+import heroVectorRight from "@/assets/hero-decor/hero-vector-right.svg";
+
+type HomeDecorProps = {
+	children: React.ReactNode;
+};
+
+export default function HomeDecor({ children }: HomeDecorProps) {
+	return (
+		<div className="relative isolate overflow-visible">
+			{/* Shared light background behind Hero + Events */}
+			<div
+				className="pointer-events-none absolute inset-x-0 top-0 z-0 h-[calc(100svh+52rem)] hack-background-gradient"
+				aria-hidden
+			/>
+
+			<img
+				src={bigCircleLeft}
+				alt=""
+				aria-hidden
+				draggable={false}
+				className="pointer-events-none absolute top-35 left-30 z-10 w-[clamp(220px,24vw,520px)] -translate-x-[34%] -translate-y-[38%] select-none opacity-80"
+			/>
+			<img
+				src={smallCircle}
+				alt=""
+				aria-hidden
+				draggable={false}
+				className="pointer-events-none absolute top-92 left-82 z-10 w-[clamp(32px,7vw,120px)] -translate-x-[34%] -translate-y-[38%] select-none opacity-80"
+			/>
+			<img
+				src={heroVectorRight}
+				alt=""
+				aria-hidden
+				draggable={false}
+				className="pointer-events-none absolute top-0 right-0 z-10 w-[clamp(200px,29vw,440px)] translate-x-[30%] -translate-y-[10%] select-none"
+			/>
+
+			{/* Spillover assets: anchor near Hero bottom so they remain visible into Events */}
+			<img
+				src={heroVectorLeft}
+				alt=""
+				aria-hidden
+				draggable={false}
+				className="pointer-events-none absolute left-0 top-[calc(100svh+2rem)] z-10 w-[clamp(200px,29vw,440px)] -translate-x-[20%] select-none"
+			/>
+			<img
+				src={bigCircleRight}
+				alt=""
+				aria-hidden
+				draggable={false}
+				className="pointer-events-none absolute right-38 top-[calc(100svh+6rem)] z-10 w-[clamp(320px,30vw,760px)] translate-x-[36%] select-none opacity-70"
+			/>
+
+			<div className="relative z-20">{children}</div>
+		</div>
+	);
+}
+
