cf2ff5528c4d87ad4cdf195620993476e406e674
Nathan Ong <nathanong1217@gmail.com>
2026-04-30 17:02:28 -0700

    feat: blobs for the background

--- client/src/pages/sponsors/Sponsors.tsx
+++ client/src/pages/sponsors/Sponsors.tsx
@@ -4,10 +4,58 @@ import SponsorUs from "./components/SponsorUs";
 
 export default function Sponsors() {
 	return (
-		<main className="min-h-screen bg-dark-blue">
+		<main className="relative min-h-screen overflow-hidden bg-dark-blue">
+      			<img
+				src="/background/green-circle.svg"
+				alt=""
+				aria-hidden="true"
+				className="pointer-events-none absolute -left-24 md:-left-5 top-20 z-0 w-90 select-none md:block"
+			/>
+
+			<img
+				src="/background/blue-blob.svg"
+				alt=""
+				aria-hidden="true"
+				className="pointer-events-none absolute -right-32 top-180 z-0 w-250 select-none md:block"
+			/>
+
+			<img
+				src="/background/green-blob.svg"
+				alt=""
+				aria-hidden="true"
+				className="pointer-events-none absolute -left-45 md:-left-100 -bottom-45 md:bottom-75 z-0 w-200 select-none md:block"
+			/>
+
+			<img
+				src="/background/email-icon.svg"
+				alt=""
+				aria-hidden="true"
+				className="pointer-events-none absolute right-80 top-[62%] z-0 hidden w-100 select-none md:block"
+			/>
+
+			<img
+				src="/background/person-icon.svg"
+				alt=""
+				aria-hidden="true"
+				className="pointer-events-none absolute right-6 top-[74%] z-0 hidden w-100 select-none md:block"
+			/>
+      <img
+        src="/background/blue-circle-outline.svg"
+        alt=""
+        aria-hidden="true"
+        className="pointer-events-none absolute -bottom-75 -left-8 z-0 hidden w-150 select-none md:block"
+        />
+      <img
+        src="/background/green-circle-outline.svg"
+        alt=""
+        aria-hidden="true"
+        className="pointer-events-none absolute -bottom-100 -right-16 z-0 hidden w-150 select-none md:block"
+        />
+      <div className="relative z-20">
 			<SponsorHero />
 			<SponsorGrid />
 			<SponsorUs />
+      </div>
 		</main>
 	);
 }
