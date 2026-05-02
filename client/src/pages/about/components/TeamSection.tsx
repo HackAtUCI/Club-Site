699251a8c3b792478ab8fe148a5b5746ccb18f99
Nathan Ong <nathanong1217@gmail.com>
2026-05-01 20:41:12 -0700

    organization

--- client/src/pages/about/components/TeamSection.tsx
+++ client/src/pages/about/components/TeamSection.tsx
@@ -28,17 +28,17 @@ export default function TeamSection() {
 			className="flex justify-center items-center text-[#2D2D3E] px-6 md:px-10 pb-30 lg:px-46"
 		>
 			<div className="container z-40">
-				<div className="flex flex-col gap-16 rounded-[40px] px-4 md:px-10 py-16 bg-linear-to-b from-[#ECEFFD] to-[#B7C2F3]">
+				<div className="flex flex-col gap-16 rounded-4xl px-4 md:px-10 py-16 hack-white-gradient">
 					<div className="flex justify-center">
-						<h1 className="text-4xl md:text-5xl font-bold md:mb-4 bg-linear-to-b from-[#2D2D3E] via-[#2D2D3E] to-[#4D4D70E5] bg-clip-text text-transparent ">
+						<h1 className="text-heading font-bold md:mb-4 gunmetal-text-gradient">
 							Our Team
 						</h1>
 					</div>
 					{Object.entries(teamMembers).map(
 						([teamName, members]: [string, TeamMember[]]) => (
-							<section key={teamName} className="w-full p-4 md:p-10 rounded-[40px] border border-white/10 bg-white/20 backdrop-blur-lg shadow-[0_3px_3px_0px_#00000040,inset_0_3px_3px_0px_#00000040]">
+							<section key={teamName} className="w-full p-4 md:p-10 rounded-4xl border border-white/10 box-shadow">
 								<div className="flex items-center justify-center gap-2 mb-6">
-									<h2 className="text-2xl md:text-3xl font-bold">
+									<h2 className="text-2xl md:text-3xl text-gunmetal font-bold">
 										{teamName}
 									</h2>
 								</div>
